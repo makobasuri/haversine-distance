2aa7815c200d43770428a62cbb35961bf8e90752
Marko Bajkovic <marko.bajkovic@unymira.com>
2021-04-09 08:18:12 +0200

    add amd support, limit possible inputs, code style

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const asin = Math.asin
-const cos = Math.cos
-const sin = Math.sin
-const sqrt = Math.sqrt
-const PI = Math.PI
+(function(root, factory) {
+  "use strict";
+  if (typeof define === 'function' && define.amd) {
+      define('circleToPolygon', factory)
+  } else if (typeof module === 'object' && module.exports) {
+      module.exports = factory()
+  } else {
+      root.circleToPolygon = factory()
+  }
+}(this, function() {
 
-// equatorial mean radius of Earth (in meters)
-const R = 6378137
+	// equatorial mean radius of Earth (in meters)
+	const R = 6378137;
 
-function squared (x) { return x * x }
-function toRad (x) { return x * PI / 180.0 }
-function hav (x) {
-  return squared(sin(x / 2))
-}
+	const squared = (x) => x * x
+	const toRad = (x) => x * Math.PI / 180.0
+	const hav = (x) => squared(Math.sin(x / 2))
 
-// hav(theta) = hav(bLat - aLat) + cos(aLat) * cos(bLat) * hav(bLon - aLon)
-function haversineDistance (a, b) {
-  const aLat = toRad(Array.isArray(a) ? a[1] : a.latitude || a.lat)
-  const bLat = toRad(Array.isArray(b) ? b[1] : b.latitude || b.lat)
-  const aLng = toRad(Array.isArray(a) ? a[0] : a.longitude || a.lng || a.lon)
-  const bLng = toRad(Array.isArray(b) ? b[0] : b.longitude || b.lng || b.lon)
+	// hav(theta) = hav(bLat - aLat) + cos(aLat) * cos(bLat) * hav(bLon - aLon)
+	return function haversineDistance(a, b) {
+		const aLat = toRad(a.latitude || a.lat)
+		const bLat = toRad(b.latitude || b.lat)
+		const aLng = toRad(a.longitude || a.lng || a.lon)
+		const bLng = toRad(b.longitude || b.lng || b.lon)
+		const ht = hav(bLat - aLat) + Math.cos(aLat) * Math.cos(bLat) * hav(bLng - aLng)
 
-  const ht = hav(bLat - aLat) + cos(aLat) * cos(bLat) * hav(bLng - aLng)
-  return 2 * R * asin(sqrt(ht))
-}
-
-module.exports = haversineDistance
+		return 2 * R * Math.asin(Math.sqrt(ht))
+	}
+}))
